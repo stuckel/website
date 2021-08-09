9bcc0e5315056f1fb3cf78de65f29c5f0ae4c690
stuckel <s_simon@gmx.ch>
2021-08-09 09:34:32 +0200

    c

---
+++ help.R
@@ -0,0 +1,16 @@
+install.packages("blogdown") # install the blogdown package
+
+library(blogdown) # load blogdown
+new_site(theme = "wowchemy/starter-academic") 
+
+file.edit("gitignore")
+
+##add lines to gitignore file
+# .Rproj.user
+# .Rhistory
+# .RData
+# .Ruserdata
+# .DS_Store
+# Thumbs.db
+
+blogdown::check_site()
